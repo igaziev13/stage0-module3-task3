2a6c801a2d03e053abdbb8baea0fa97f93e1539c
IGAZIEV <islam.gaziev@gmail.com>
2022-08-03 15:23:36 +0500

    Module #3. Task #3. Attempt #4.

--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -2,24 +2,26 @@ package lang.print.gaps.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        int first = 1;
-        int second = 10;
-        int third = 100;
+        //int declaration
+        int first = 1, second = 10, third = 100;
+        //int output
         System.out.println(first);
         System.out.println(second);
         System.out.println(third);
+        //var declaration
         var linkToFirst = first;
         var linkToSecond = second;
         var linkToThird = third;
+        //int re-assignment
         first = 15;
         second = 6;
         third = 4;
+        //all variables output
         System.out.println(first);
         System.out.println(second);
         System.out.println(third);
         System.out.println(linkToFirst);
         System.out.println(linkToSecond);
         System.out.println(linkToThird);
-        //write code here
     }
 }
