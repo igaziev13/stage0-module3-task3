c579d66eeb1c535926d65fcce2101c48eba7686a
IGAZIEV <islam.gaziev@gmail.com>
2022-08-03 16:02:45 +0500

    Module #3. Task #3. Attempt #8.

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -2,10 +2,7 @@ package lang.print.gaps.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        int first, second, third;
-        first = 1;
-        second = 10;
-        third = 100;
+        int first = 1, second = 10, third = 100;
         System.out.println(first);
         System.out.println(second);
         System.out.println(third);
